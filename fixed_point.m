bb302712f73c83d6550b4d8e48e15c3a11e5ba6b
Kelvin Novais <kelvin.novais@protonmail.com>
2024-03-09 23:14:46 -0300

    Added fixed point method function

---
+++ fixed_point.m
@@ -0,0 +1,30 @@
+function [p] = fixed_point (p0, TOL, N, g)
+  i = 1;
+  pi = p0;
+
+  printf ("\n\nFIXED POINT METHOD\n");
+  printf ("│ %-15.15s │ %-15.15s │ %-15.15s │ %-15.15s │\n",
+          "i", "pi", "g(pi)", "|pi - p0|"
+  );
+
+  while (i <= N)
+    gp = g(pi);
+
+    printf ("│ %-15d │ %-15d │ %-15d │ %-15d │\n",
+            i, pi, gp, (abs (gp - pi))
+    );
+
+    if ((abs (gp - pi)) < TOL)
+      printf ("\nFound root:\n");
+      ROOT = gp
+      printf ("With an error of:\n");
+      ERROR = (abs (gp - pi))
+      return;
+    endif
+
+    i += 1;
+    pi = gp;
+  endwhile
+
+  error ('Method failed after N = %d iterations', N);
+endfunction
